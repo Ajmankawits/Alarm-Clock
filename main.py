5dff77e158dafcac4be944132d69b4958e0317f9
Alex <thatsoalex@gmail.com>
2022-03-18 15:54:25 -0500

    Changed main.py to only have one object and call the relevant functions

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from alarm import Alarm
 
-current_time = Alarm()
-current_time.current_time()
-
-change_time = Alarm()
-change_time.set_time()
-
-toggle_clock = Alarm()
-toggle_clock.toggle_alarm() 
-
-set_alarm = Alarm()
-set_alarm.set_alarm()
+alarm_clock = Alarm()
+alarm_clock.current_time()
+alarm_clock.set_time()
+alarm_clock.toggle_alarm() 
+alarm_clock.set_alarm()
